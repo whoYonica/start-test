263afe465633138f2e15e035716774714e414dce
Elena Tsibikova <eladielkach@gmail.com>
2026-06-24 18:49:42 +0500

    new code

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,9 +1,4 @@
 from capitalize import capitalize
 
-if capitalize("hello") != "Hello":
-    raise Exception("Функция работает неверно!")
-
-if capitalize("") != "":
-    raise Exception("Функция работает неверно!")
-
-print("Все тесты пройдены!")
+assert capitalize("") == ""
+assert capitalize("hello") == "Hello"
